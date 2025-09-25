8cf17a9fd0dbcb842d257d416c306f2a5ed125ec
duandi <1434081924@qq.com>
2025-09-25 14:58:59 +0800

    插入数据2

--- migrations/0001_create_user_table.sql
+++ migrations/0001_create_user_table.sql
@@ -8,8 +8,8 @@ CREATE TABLE "User" (
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
-INSERT INTO "User" ("email", "name") VALUES ('jane@prisma.io', 'Jane Doe');
-INSERT INTO "User" ("email", "name") VALUES ('jane@prisma.io', 'Jane Doe');
-INSERT INTO "User" ("email", "name") VALUES ('jane@prisma.io', 'Jane Doe');
-INSERT INTO "User" ("email", "name") VALUES ('jane@prisma.io', 'Jane Doe');
-INSERT INTO "User" ("email", "name") VALUES ('jane@prisma.io', 'Jane Doe');
+INSERT INTO "User" ("email", "name") VALUES ('jane@prisma1.io', 'Jane Doe1');
+INSERT INTO "User" ("email", "name") VALUES ('jane@prisma2.io', 'Jane Doe2');
+INSERT INTO "User" ("email", "name") VALUES ('jane@prisma3.io', 'Jane Doe');
+INSERT INTO "User" ("email", "name") VALUES ('jane@prisma4.io', 'Jane Doe4');
+INSERT INTO "User" ("email", "name") VALUES ('jane@prisma5.io', 'Jane Doe5');
